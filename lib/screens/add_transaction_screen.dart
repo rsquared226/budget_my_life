17b7dd280ec0a017022ef6f6bf10106265e64511
Rahul Ramkumar <rahulram226@gmail.com>
2020-05-28 20:15:48 -0400

    Add FAB

---
+++ lib/screens/add_transaction_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// This is what pops up when the FAB on home_screen is clicked.
+
+class AddTransactionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: const Text('Add transaction screen placeholder'),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:animations/animations.dart';
 
+import './add_transaction_screen.dart';
 import './graphs_screen.dart';
 import './history_screen.dart';
+
+// Need this because the built-in FloatingActionButton widget isn't being used. A custom one is for the animation.
+const _fabDimension = 56.0;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -32,6 +36,30 @@ class _HomeScreenState extends State<HomeScreen> {
           );
         },
         child: _pageList[_pageIndex],
+      ),
+      floatingActionButton: OpenContainer(
+        closedElevation: 6.0,
+        closedShape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(_fabDimension / 2),
+          ),
+        ),
+        closedColor: Theme.of(context).primaryColor,
+        closedBuilder: (context, _) {
+          return SizedBox(
+            height: _fabDimension,
+            width: _fabDimension,
+            child: Center(
+              child: Icon(
+                Icons.add,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+            ),
+          );
+        },
+        openBuilder: (_, __) {
+          return AddTransactionScreen();
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _pageIndex,
