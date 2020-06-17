84e434cecf50af8c8e92c662d74d8de1ce397472
Rahul Ramkumar <rahulram226@gmail.com>
2020-06-16 21:23:42 -0400

    Add Dialog to Confirm Transaction Deletion Also extract edit and deletion functions

--- lib/screens/transaction_details_screen.dart
+++ lib/screens/transaction_details_screen.dart
@@ -13,6 +13,60 @@ class TransactionDetailsScreen extends StatelessWidget {
 
   const TransactionDetailsScreen({@required this.transactionId});
 
+  void onEditTransaction(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => EditTransactionScreen(
+          closeContainer: () => Navigator.pop(context),
+          editTransactionId: transactionId,
+        ),
+      ),
+    );
+  }
+
+  Future<void> onDeleteTransaction(
+      BuildContext context, Function deleteTransaction) async {
+    if (!(await confirmDeleteTransaction(context))) {
+      return;
+    }
+    deleteTransaction(transactionId);
+    // Doing this will avoid animation errors. It pops everything and goes back to the homescreen so the
+    // closeContainer animation won't play on a deleted transaction.
+    Navigator.pushNamedAndRemoveUntil(context, '/', (_) => false);
+  }
+
+  // Return true if the user wants to delete, false if they cancel.
+  Future<bool> confirmDeleteTransaction(BuildContext context) async {
+    final confirmDelete = await showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Delete transaction?'),
+          content:
+              const Text('Are you sure you want to delete this transaction?'),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+              child: const Text('Cancel'),
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(context, true);
+              },
+              child: const Text('Delete'),
+            ),
+          ],
+        );
+      },
+    );
+    // If the user clicks out of the dialog to dismiss, the result will be null. We will assume they don't want to
+    // delete the transaction if they do that.
+    return confirmDelete ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
     final appTheme = Theme.of(context);
@@ -30,24 +84,11 @@ class TransactionDetailsScreen extends StatelessWidget {
         TransactionDetailsAppBar(
           transactionAmount: transaction.amount,
           formattedAmount: transaction.formattedAmount,
-          editTransaction: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) => EditTransactionScreen(
-                  closeContainer: () => Navigator.pop(context),
-                  editTransactionId: transactionId,
-                ),
-              ),
-            );
-          },
-          deleteTransaction: () {
-            // TODO: add "are you sure" dialogue.
-            transactionsData.deleteTransaction(transactionId);
-            // Doing this will avoid animation errors. It pops everything and goes back to the homescreen so the
-            // closeContainer animation won't play on a deleted transaction.
-            Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
-          },
+          editTransaction: () => onEditTransaction(context),
+          deleteTransaction: () => onDeleteTransaction(
+            context,
+            transactionsData.deleteTransaction,
+          ),
         ),
         SliverPadding(
           padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
