560c9d46b4d2a5fe8b972067d27d429e306c3c1e
Rahul Ramkumar <rahulram226@gmail.com>
2020-07-18 11:37:02 -0400

    Make Basic Labels Pie Chart

---
+++ lib/charts/chart_widgets/labels_pie_chart.dart
@@ -0,0 +1,36 @@
+import 'package:budget_my_life/charts/chart_models/pie_chart_model.dart';
+import 'package:budget_my_life/charts/charts_base/pie_chart_base.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/label.dart';
+import '../../providers/labels.dart';
+
+class LabelsPieChart extends StatelessWidget {
+  final LabelType labelType;
+
+  const LabelsPieChart({
+    @required this.labelType,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final labelsData = Provider.of<Labels>(context, listen: false);
+    final labels = labelType == LabelType.INCOME
+        ? labelsData.incomeLabels
+        : labelsData.expenseLabels;
+
+    return PieChartBase(
+      id: labelType.toString(),
+      pieData: labels
+          .map(
+            (label) => PieChartModel(
+              label: label.title,
+              amount: label.getLabelTotal(context),
+              color: label.color,
+            ),
+          )
+          .toList(),
+    );
+  }
+}
--- lib/screens/graphs_screen.dart
+++ lib/screens/graphs_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../models/label.dart';
+import '../charts/chart_widgets/labels_pie_chart.dart';
+
 // This screen is a screen under home_screen
 
 class GraphsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: const Text('Graphs screen placeholder'),
+      child: const LabelsPieChart(labelType: LabelType.INCOME),
     );
   }
 }
