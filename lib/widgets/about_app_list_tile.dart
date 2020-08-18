77e4e6a4d159a5e925823205a3276dd9eafeb120
Rahul Ramkumar <rahulram226@gmail.com>
2020-08-18 10:07:37 -0400

    Version Bump

--- lib/widgets/about_app_list_tile.dart
+++ lib/widgets/about_app_list_tile.dart
@@ -35,7 +35,7 @@ class AboutAppListTile extends StatelessWidget {
         width: 75,
       ),
       applicationLegalese: 'Made by Rahul Ramkumar, licensed under GNU GPLv3',
-      applicationVersion: 'v0.4.0',
+      applicationVersion: 'v0.4.3',
       aboutBoxChildren: <Widget>[
         const SizedBox(height: 12),
         const Text('A beautiful and informative budgeting app'),
