5e59d07fbcedf54b1c9b8e972d54bf34d47cef13
Rahul Ramkumar <rahulram226@gmail.com>
2020-08-22 09:43:51 -0400

    Create Onboarding

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -6,6 +6,13 @@ import '../utils/db_helper.dart';
 class Onboarding extends StatelessWidget {
   static const routeName = '/onboarding';
 
+  Widget buildImage(String assetName) {
+    return Align(
+      child: Image.asset('assets/images/$assetName.jpg', width: 350.0),
+      alignment: Alignment.bottomCenter,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     const pageDecoration = const PageDecoration(
@@ -15,15 +22,40 @@ class Onboarding extends StatelessWidget {
       pageColor: Colors.white,
       imagePadding: EdgeInsets.zero,
     );
+
     final introPages = <PageViewModel>[
       PageViewModel(
-        title: 'Test',
-        body: 'Test12',
+        image: buildImage('Intro1'),
+        title: 'Welcome to Budget My Life!',
+        body: 'Here are a few things you should know to get started',
         decoration: pageDecoration,
       ),
       PageViewModel(
-        title: 'Test',
-        body: 'Test12',
+        image: buildImage('Intro2'),
+        title: 'Adding Transactions',
+        body:
+            'Click on the button in the bottom right of the dashboard screen to add a transaction!',
+        decoration: pageDecoration,
+      ),
+      PageViewModel(
+        image: buildImage('Intro3'),
+        title: 'Customize Labels',
+        body:
+            'Open the drawer to customize labels. These help provide you with insights on how you earn/spend your money',
+        decoration: pageDecoration,
+      ),
+      PageViewModel(
+        image: buildImage('Intro4'),
+        title: 'View Insights',
+        body:
+            'Click on the second bottom tab on the main screen to view your insights!',
+        decoration: pageDecoration,
+      ),
+      PageViewModel(
+        image: buildImage('Intro5'),
+        title: 'Get Started!',
+        body:
+            'You can always view this menu again by tapping "Help" in the drawer',
         decoration: pageDecoration,
       ),
     ];
