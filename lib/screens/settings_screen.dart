8fa42554cb1d61f0a4641e8c656eb036f3a7e737
Rahul Ramkumar <rahulram226@gmail.com>
2020-11-25 15:42:06 -0500

    Disable Change Currency Symbol When not Shown

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -8,6 +8,25 @@ import '../widgets/app_drawer.dart';
 class SettingsScreen extends StatelessWidget {
   static const routeName = '/settings';
 
+  // All this is for simulating a disabled setting when the checkbox is unchecked.
+  Widget _buildSettingDisabled({
+    @required BuildContext context,
+    @required bool disabled,
+    @required Widget child,
+  }) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 125),
+      foregroundDecoration: BoxDecoration(
+        color: !disabled ? null : Theme.of(context).canvasColor.withOpacity(.6),
+      ),
+      child: IgnorePointer(
+        ignoring: disabled,
+        // This is the actual setting.
+        child: child,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final settingsProvider = Provider.of<Settings>(context);
@@ -30,17 +49,23 @@ class SettingsScreen extends StatelessWidget {
                 onChanged: (changedShowCurrency) =>
                     settingsProvider.showCurrency = changedShowCurrency,
               ),
-              SettingTextItem(
-                title: 'Change Currency Symbol',
-                onChanged: (changedCurrencySymbol) {
-                  if (changedCurrencySymbol.isEmpty) {
-                    // TODO: show snackbar or something
-                    return;
-                  }
-                  settingsProvider.currencySymbol = changedCurrencySymbol;
-                },
-                displayValue: settingsProvider.currencySymbol,
-                initialValue: settingsProvider.currencySymbol,
+              _buildSettingDisabled(
+                context: context,
+                disabled: !settingsProvider.showCurrency,
+                child: SettingTextItem(
+                  title: 'Change Currency Symbol',
+                  onChanged: (changedCurrencySymbol) {
+                    if (changedCurrencySymbol == null ||
+                        changedCurrencySymbol.isEmpty ||
+                        changedCurrencySymbol.length > 5) {
+                      // Don't change anything if the users clicks cancel or if they input nothing in the box.
+                      return;
+                    }
+                    settingsProvider.currencySymbol = changedCurrencySymbol;
+                  },
+                  displayValue: settingsProvider.currencySymbol,
+                  initialValue: settingsProvider.currencySymbol,
+                ),
               ),
             ],
           ),
