bf5a156bb1ba7d805bd15e03749e6af13bebbfb6
Rahul Ramkumar <rahulram226@gmail.com>
2020-12-22 11:11:12 -0500

    Start Dark Theme

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider with ChangeNotifier {
+  static final lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.indigo,
+    accentColor: Colors.deepPurpleAccent,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    // This page transition looks better than the default.
+    pageTransitionsTheme: const PageTransitionsTheme(
+      builders: <TargetPlatform, PageTransitionsBuilder>{
+        TargetPlatform.android: ZoomPageTransitionsBuilder(),
+      },
+    ),
+  );
+
+  static final darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.indigo,
+    accentColor: Colors.deepPurpleAccent,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    // This page transition looks better than the default.
+    pageTransitionsTheme: const PageTransitionsTheme(
+      builders: <TargetPlatform, PageTransitionsBuilder>{
+        TargetPlatform.android: ZoomPageTransitionsBuilder(),
+      },
+    ),
+  );
+}
